db1346273b3d4ed2d6605c7e91e7e3df2f84a451
Kathan Naik <115002064+KathanNaik@users.noreply.github.com>
2023-11-13 16:27:31 +0530

    Add files via upload

--- Email Signature Parsing/app.py
+++ Email Signature Parsing/app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def home():
     return 'Kathan Here!'
 
-#Email Credentials
 f = open("email_cred.json", "r")
 
 cred = json.load(f)
@@ -20,7 +19,6 @@ email_password = cred.get("email_password")
 
 print(cred);
 
-#Email Config
 app.config['MAIL_SERVER']='smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = email_address
@@ -29,7 +27,6 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
-#Reading Key
 f = open("key.txt", "r")
 
 openai.api_key = f.readline()
@@ -37,7 +34,6 @@ openai.api_base = "https://api.pulze.ai/v1"
 
 f.close()
 
-#Defining System Msg
 instructiones = '''User will give an email text as message content. Your task is to retrive email signature from that email. You should follow below steps 
 1) figure out the name of person sending the mail
 2) find the name of the organization or company the person belongs to if mentioned any
@@ -48,9 +44,8 @@ instructiones = '''User will give an email text as message content. Your task is
         "org": "organization/company"
         "number": mobile number found
     }
-5) you can keep fields empty by putting 'Null' if you do not find any of them'''
+5) you can keep fields empty by putting 'NULL' if you do not find any of them'''
 
-#Pulze API call
 def pulze_llm(mail):
     chat_response = openai.ChatCompletion.create(
         model="pulze-v0",
@@ -64,16 +59,46 @@ def pulze_llm(mail):
     print(chat_response)
     return chat_response.choices[0].message.content
 
-#Request Processing
+def pulze_llm_p(mail, list):
+    
+    listMsg = (
+        '''Given is the list of product name and their description separated by an equal to/= sign'''
+            
+        + list + 
+            
+        '''
+        1) Your task is to figure out if the user has mentioned any of the products in their message.
+        2) Return the name of the product in JSON format as below:
+        {
+            "product": "the product" 
+        }
+        4) Strictly include only one product name given in the list as the product.
+        3) You can keep fields empty by putting 'NULL' if you do not find any of them.
+        '''
+    )
+    
+    chat_response = openai.ChatCompletion.create(
+        
+        model="pulze-v0",
+        max_tokens=100,
+        messages=[
+            {"role": "system", "content": listMsg},
+            {"role": "user", "content": mail}
+        ],
+    )
+
+    print(chat_response)
+    return chat_response.choices[0].message.content
+
 @app.route("/mailpost", methods=["POST"])
-def login():
+def signatureParsing():
     if request.method == "POST":
         user_address = request.form["address"]
         user_email = request.form["email"]
         access_password = request.form["password"]
         
         if(access_password == "Aethereus@123"):
-             msg = Message( 
+            msg = Message( 
                 'Email-Conformation', 
                 sender = email_address, 
                 recipients = [user_address] 
@@ -87,5 +112,20 @@ def login():
         else:
             return "invalid request"
         
-        if __name__ == "__main__":
+@app.route("/product", methods=["POST"])
+def productParsing():
+    if request.method == "POST":
+        user_email = request.form["email"]
+        product_list = request.form["products"]
+        access_password = request.form["password"]
+        
+        if(access_password == "Aethereus@123"):
+            
+            r = Response(response=pulze_llm_p(user_email, product_list), status=200, mimetype="application/json")
+            r.headers["Content-Type"] = "application/json; charset=utf-8"
+            return r
+        else:
+            return "invalid request"
+        
+if __name__ == "__main__":
     app.run(host="localhost", port=8080)
